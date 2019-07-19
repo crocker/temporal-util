d01143f04c065faec09358d2c1f04ca489ef6c93
Jason Crocker <jcrocker@logicstack.com>
2019-07-19 18:29:06 -0400

    Reverted previous commit

--- src/main/scala/com/logicstack/util/temporal/model/schedule/Schedule.scala
+++
@@ -1,8 +0,0 @@
-package com.logicstack.util.temporal.model.schedule
-
-import java.time.LocalTime
-
-//  {"sunday":[["9:00","21:00"]],"saturday":[["8:00","23:00"]],"tuesday":[["9:00","22:00"]],"friday":[["9:00","23:00"]],"thursday":[["9:00","22:00"]],"wednesday":[["9:00","22:00"]],"monday":[["9:00","22:00"]]}
-case class Schedule(
-  events: Seq[Event] = Seq.empty
-)
